66d544ffe479f8e019c2f5846682265049530767
Giftoncandy <giftoncandy00@gmail.com>
2023-04-22 12:26:01 -0700

    Hello-world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but with the outcome is a piece of art, \n");
+	return (0)
+}
